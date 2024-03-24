4d7b90d141145d1291fd8eb66bd6f20d910c4ad6
Atharva-Deshmukh1 <atharva25deshmukh@gmail.com>
2024-03-24 15:05:42 +0530

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useTheme } from "../../ThemeContext";
+import './Navbar.css';
 
 const Navbar = () => {
   
-  const {theme,toggleTheme} = useTheme()
+  const {theme, toggleTheme} = useTheme();
 
   return (
-    <div>
-    <div>
-    <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/About'>About</Link>
-      <Link to='/LogIn'>LogIn</Link>
-    </nav>
-    </div>
-    <div className="mode-switch">
-      <label>
-      <input type="checkbox"
-      onClick={toggleTheme}
-      checked={theme === "dark"} />
-      </label>
-    </div>
-    
+    <div className="navbar-container">
+      <div className="nav-links">
+        <nav>
+          <Link to='/' className="nav-link">Home</Link>
+          <Link to='/About' className="nav-link">About</Link>
+          <Link to='/LogIn' className="nav-link">LogIn</Link>
+        </nav>
+      </div>
+      <div className="mode-switch">
+        <label className="switch">
+          <input 
+            type="checkbox"
+            onClick={toggleTheme}
+            checked={theme === "dark"}
+          />
+          <span className="slider"></span>
+        </label>
+      </div>
     </div>
   );
 };
