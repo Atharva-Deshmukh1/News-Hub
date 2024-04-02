c615b711b230c6e055f039f30f1785e5a19b80d7
Atharva-Deshmukh1 <atharva25deshmukh@gmail.com>
2024-04-02 19:09:19 +0530

    update

---
+++ src/Components/SavedNews/SavedNews.jsx
@@ -0,0 +1,40 @@
+
+import React, { useContext } from 'react';
+import { SavedNewsContext } from '../SavedNewsContext/SavedNewsContext';
+import { Link } from 'react-router-dom';
+import { useTheme } from '../NewsContext/NewsContext'; // Import the useTheme hook
+import '../SavedNews/SavedNews.css';
+
+const SavedNews = () => {
+  const { savedNews, removeNews } = useContext(SavedNewsContext);
+  const { theme } = useTheme(); // Use the useTheme hook
+
+  const handleRemove = (index) => {
+    removeNews(index);
+  };
+
+  return (
+    <div className={`saved-news-container ${theme}`}>
+      <h1>Saved News</h1>
+      <ul>
+        {savedNews.map((item, index) => (
+          <li key={index}>
+            <img src={item.urlToImage} alt={item.title} className="saved-image" />
+            <a className={`link ${theme}`} href={item.url} target="_blank" rel="noopener noreferrer">
+              {item.title}
+            </a>
+            <button onClick={() => handleRemove(index)} className="remove-btn">
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+      <Link to="/" className="back-btn">
+        Back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default SavedNews;
+
